05372252ab7877523a066131fee9eb89190121cc
Rajkumar <rajutaya@gmail.com>
2019-11-11 19:22:15 +0530

    Fix :config changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
       port:                 587,
       domain:               'gmail.com',
       user_name:            'xxxx@gmail.com',
-      password:             'testtest',
+      password:             'xxx',
       authentication:       'plain',
       enable_starttls_auto: true  }
 
